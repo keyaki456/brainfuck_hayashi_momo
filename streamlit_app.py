eaa234f626224040ce9a1b5b17bfc2fa4a0bd880
tamiyokeyaki <122135530+keyaki456@users.noreply.github.com>
2024-10-28 11:47:56 +0000

    あいうえお

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,31 +3,50 @@ import time
 
 st.title("BrainFuck林もも")
 text = st.text_input("Your BrainFuck林もも here", "ママでちゅよ")
+input = st.text_input("標準入力")
 
 on = st.toggle("←runボタン。これがonの間、有林が働き続ける。")
 
 cells = [0] * 256
 cursol=0
+workinghayashi=0
 
 yuuhayashi=text
-if on:
-    yuuhayashi=yuuhayashi.replace('ママでちゅよ', '7')
-    yuuhayashi=yuuhayashi.replace('有林', '8')
-    yuuhayashi=yuuhayashi.replace('おっぱい', '+')
-    yuuhayashi=yuuhayashi.replace('ボロン', '-')
-    yuuhayashi=yuuhayashi.replace('妹ちゃん', '.')
-    yuuhayashi=yuuhayashi.replace('護法2', ',')
-    yuuhayashi=yuuhayashi.replace('アピールしてください', '[')
-    yuuhayashi=yuuhayashi.replace('こんもも', ']')
+yuuhayashi=yuuhayashi.replace('ママでちゅよ', '右')
+yuuhayashi=yuuhayashi.replace('有林', '左')
+yuuhayashi=yuuhayashi.replace('おっぱい', '足')
+yuuhayashi=yuuhayashi.replace('ボロン', '引')
+yuuhayashi=yuuhayashi.replace('妹ちゃん', '出')
+yuuhayashi=yuuhayashi.replace('護法2', '入')
+yuuhayashi=yuuhayashi.replace('アピールしてください', '恥')
+yuuhayashi=yuuhayashi.replace('こんもも', '終')
 
-    while cursol<=len(yuuhayashi):
+if on:
+    while cursol<=(len(yuuhayashi)-1):
         time.sleep(0.2)
-        st.write("ありはやし")
+        if yuuhayashi[cursol]=="右":
+            workinghayashi=workinghayashi+1
+            if workinghayashi==256:workinghayashi=0
+            cursol=cursol+1
+        elif yuuhayashi[cursol]=='左':
+            workinghayashi=workinghayashi-1
+            if workinghayashi==-1:workinghayashi=255
+            cursol=cursol+1
+        elif yuuhayashi[cursol]=='足':
+            cells[workinghayashi]=cells[workinghayashi]+1
+            if cells[workinghayashi]==256:cells[workinghayashi]=0
+            cursol=cursol+1
+        elif yuuhayashi[cursol]=='引':
+            cells[workinghayashi]=cells[workinghayashi]-1
+            if cells[workinghayashi]==-1:cells[workinghayashi]=255
+            cursol=cursol+1
+
 
     st.write(cells)
     st.write(cells[9])
+    st.write("180秒後にもう一周します")
+    time.sleep(180)
     cells=[0] * 256
-
 
 
 
@@ -35,3 +54,4 @@ if on:
 st.write('デバッグエリア')
 st.write(text)
 st.write(yuuhayashi)
+st.write(yuuhayashi[7])
