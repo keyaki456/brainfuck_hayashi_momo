60ff209794dbe678b11b9464bfb4143dfb35e2a7
tamiyokeyaki <122135530+keyaki456@users.noreply.github.com>
2024-10-28 11:20:07 +0000

    あいうえお

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
+import time
 
 st.title("BrainFuck林もも")
 text = st.text_input("Your BrainFuck林もも here", "ママでちゅよ")
 
 on = st.toggle("←runボタン。これがonの間、有林が働き続ける。")
+
+cells = [0] * 256
+cursol=0
 
 yuuhayashi=text
 if on:
@@ -16,7 +20,18 @@ if on:
     yuuhayashi=yuuhayashi.replace('アピールしてください', '[')
     yuuhayashi=yuuhayashi.replace('こんもも', ']')
 
+    while cursol<=len(yuuhayashi):
+        time.sleep(0.2)
+        st.write("ありはやし")
+
+    st.write(cells)
+    st.write(cells[9])
+    cells=[0] * 256
 
 
+
+
+
+st.write('デバッグエリア')
 st.write(text)
 st.write(yuuhayashi)
